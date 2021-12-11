c4a0dc4f3cf53a7b7e9f43ffccef1b2a4ec612af
ghdus819 <ghdus819@gmail.com>
2021-12-11 19:58:16 +0900

    [Feat] Add Droping Present

---
+++ Assets/Scripts/PresentCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentCtrl : MonoBehaviour
+{
+    public static float speed = 0.03f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed);
+        Destroy(gameObject, 5f);
+    }
+}
---
+++ Assets/Scripts/PresentGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentGenerator : MonoBehaviour
+{
+    public GameObject presentPrefab;
+    float spawnRate = 2f, timePassed = 0f;
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+
+        if (timePassed >= spawnRate)
+        {
+            timePassed = 0f;
+            spawnRate = Random.Range(1f, 3f);
+            Instantiate(presentPrefab, transform.position, transform.rotation);
+        }
+    }
+}
