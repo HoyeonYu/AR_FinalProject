664efb0e5460bc286dbdd97377f42c6e3da21d1d
ghdus819 <ghdus819@gmail.com>
2021-12-11 19:16:53 +0900

    [Feat] Add Animation

--- Assets/Scripts/SantaCtrl.cs
+++ Assets/Scripts/SantaCtrl.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class SantaCtrl : MonoBehaviour
 {
-    public static float speed = 0.005f;
+    public static float speed = 0.01f;
+    Animator animator;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.right * speed);
-            Debug.Log("LEFT");
+
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isRun", true);
+            animator.SetBool("isJump", false);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(-1 * Vector3.right * speed);
-            Debug.Log("RIGHT");
+
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isRun", true);
+            animator.SetBool("isJump", false);
+        }
+
+        else
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isRun", false);
+            animator.SetBool("isJump", false);
         }
     }
 }
--- Assets/Scripts/ThiefCtrl.cs
+++ Assets/Scripts/ThiefCtrl.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class ThiefCtrl : MonoBehaviour
 {
-    public static float speed = 0.005f;
+    public static float speed = 0.01f;
+    Animator animator;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.right * speed);
-            Debug.Log("LEFT");
+
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isRun", true);
+            animator.SetBool("isJump", false);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(-1 * Vector3.right * speed);
-            Debug.Log("RIGHT");
+
+            animator.SetBool("isIdle", false);
+            animator.SetBool("isRun", true);
+            animator.SetBool("isJump", false);
+        }
+
+        else
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isRun", false);
+            animator.SetBool("isJump", false);
         }
     }
 }
