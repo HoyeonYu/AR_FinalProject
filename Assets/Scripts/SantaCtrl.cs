2abae64339a29bc65c54368b40f23166f453e829
ghdus819 <ghdus819@gmail.com>
2021-12-11 17:11:43 +0900

    [Feat] Add Keyboard Control

---
+++ Assets/Scripts/SantaCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaCtrl : MonoBehaviour
+{
+    public static float speed = 0.005f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.right * speed);
+            Debug.Log("LEFT");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(-1 * Vector3.right * speed);
+            Debug.Log("RIGHT");
+        }
+    }
+}
---
+++ Assets/Scripts/ThiefCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefCtrl : MonoBehaviour
+{
+    public static float speed = 0.005f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * speed);
+            Debug.Log("LEFT");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-1 * Vector3.right * speed);
+            Debug.Log("RIGHT");
+        }
+    }
+}
